7194427e5d2c68d593ef40e90d72dac88ac07481
Tanya Sashyna <sashina51@gmail.com>
2019-06-06 18:04:39 +0300

    created component selects-block

--- src/components/caption/Caption.js
+++ src/components/caption/Caption.js
@@ -3,21 +3,27 @@ import React from 'react';
 import "./caption.scss";
 
 import ImgComponent from '../imgComponent/ImgComponent';
+import SelectsBlock from '../selects-block/SelectsBlock';
 
 export default class Caption extends React.Component {
     render(){
         return(
-            <div className='from-to'>
-                <div className="caption-wrap">
-                    <h2 className="caption">Пассажирские перевозки</h2>
-                    <p className="legend">
-                        Автобусные пассажирские перевозки нашим комфортабельным автобусом доставят вам удовольствие от поездки.
-                        Время в пути пройдет незаметно благодаря уютной обстановке салона и приветливому водителю.
-                    </p>
-                </div>
-                <div className="caption-img">
-                    <ImgComponent src="https://cdn.pixabay.com/photo/2017/01/20/00/30/maldives-1993704_960_720.jpg"/>
-                </div>
+            <div>
+				<div className='from-to'>
+					<div className="caption-wrap">
+						<h2 className="caption">Пассажирские перевозки</h2>
+						<p className="legend">
+							Автобусные пассажирские перевозки нашим комфортабельным автобусом доставят вам удовольствие от поездки.
+							Время в пути пройдет незаметно благодаря уютной обстановке салона и приветливому водителю.
+						</p>
+					</div>
+					<div className="caption-img">
+						<ImgComponent src="https://cdn.pixabay.com/photo/2017/01/20/00/30/maldives-1993704_960_720.jpg"/>
+					</div>
+				</div>
+				<div>
+					<SelectsBlock/>
+				</div>
             </div>
         )
     }
---
+++ src/components/selects-block/SelectsBlock.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class SelectsBlock extends React.Component {
+	render(){
+		return(
+			<select>
+				<option>Some text1</option>
+				<option>Some text2</option>
+			</select>
+		)
+	}
+}
